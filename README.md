8c1e1afb614cf43cdc40280a05bb8a082aaf8f09
Kleiner Hacker <Christoph.Pfeiffer.Berlin@freenet.de>
2021-11-05 17:43:37 +0100

    initial commit

--- README.md
+++ README.md
@@ -1,2 +1,18 @@
 # unity-game-tooling
 Game Toolings for Unity
+
+# install
+Use this repository directly in Unity.
+
+### Dependencies
+* https://github.com/KleinerHacker/unity-common-ex
+* https://github.com/KleinerHacker/unity-animation
+
+### Open UPM
+URL: https://package.openupm.com
+
+Scope: org.pcsoft
+
+# usage
+* `FlyingPanningZoomCameraController` is a camera made for simulation views. 
+Use it always with `CameraBorder` to setup scrolling borders.
---
+++ Scripts/UnityGameToolingConstants.cs
@@ -0,0 +1,22 @@
+namespace UnityGameTooling.Runtime.game_tooling.Scripts
+{
+    public static class UnityGameToolingConstants
+    {
+        public const string Root = "Unity Game Tooling";
+
+        public static class Menu
+        {
+            public static class Asset
+            {
+                private const string RootMenu = Root;
+            }
+
+            public static class Component
+            {
+                private const string RootMenu = Root;
+
+                public const string CameraMenu = RootMenu + "/Camera";
+            }
+        }
+    }
+}
